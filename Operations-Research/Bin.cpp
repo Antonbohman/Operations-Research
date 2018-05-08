650b10346c04a46ece0c6e5979183a3b8b07f951
Nathanael Remstedt <narm17@student.bth.se>
2018-05-08 17:23:50 +0200

    Bin.cpp complete

--- Operations-Research/Bin.cpp
+++ Operations-Research/Bin.cpp
@@ -15,6 +15,30 @@ Bin::~Bin()
 {
 }
 
+bool Bin::addOperation(const Operation & op)
+{
+	schedule.insertAt(schedule.length(),op);
+	return true;
+}
+
+int Bin::remainingSize()
+{
+	int output = size;
+	for (int i = 0; i < schedule.length(); i++)
+	{
+		output -= schedule.getAt(i).getTime();
+	}
+	return output;
+}
+
+void Bin::emptyBin()
+{
+	for (int i = 0; i < schedule.length(); i++)
+	{
+		schedule.removeAt(0);
+	}
+}
+
 int Bin::capacity() const
 {
 	return size;
@@ -23,6 +47,15 @@ int Bin::capacity() const
 int Bin::length() const
 {
 	return schedule.length();
+}
+
+bool Bin::resize(int _size)
+{
+	if (_size >= 0) {
+		size = _size;
+		return true;
+	}
+	return false;
 }
 
 Bin & Bin::operator=(const Bin & origin)
