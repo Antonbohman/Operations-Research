a10add2f4b1f2dcdca9d4a599a55c6d439a81ce0
Nathanael Remstedt <narm17@student.bth.se>
2018-05-09 18:04:38 +0200

    Added fitsto schedule class

--- Operations-Research/Schedule.cpp
+++ Operations-Research/Schedule.cpp
@@ -17,7 +17,13 @@ Schedule::~Schedule() {
 
 }
 
-void Schedule::setTitle(const string _title) {
+void Schedule::setTitle(const string _title) 
+{
+	title = _title;
+}
+
+void Schedule::fillBins(AlgorithmType type)
+{
 }
 
 void Schedule::printSchedule(const int amountRooms, const Bin * rooms) {
@@ -31,23 +37,104 @@ Schedule & Schedule::operator=(const Schedule & origin) {
 }
 
 string Schedule::intToTime(const int time) {
-	return string();
+	string hour = to_string(time / 60);
+	if (hour.length() == 1)
+		hour = "0" + hour;
+
+	string min = to_string(time % 60);
+	if (min.length() == 1)
+		min = "0" + min;
+
+	return hour + "h" + min + "m";
 }
 
-void Schedule::nextFit(PriorityQueue<Operation>& queue, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
+void Schedule::nextFit()
+{
+	Operation curr;
+
+	int currentRoom = 0;
+
+	totalTime = 0;
+	totalOperation = 0;
+	bookedTime = 0;
+	bookedOperation = 0;
+
+	processTime = clock();
+	int index = 0;
+	int length = list.length();
+	while ((datatype == DataType::HEAP ? !queue.isEmpty() : index < length)) {
+		curr = (datatype == DataType::HEAP ? queue.dequeue() : list.getAt(index++));
+		totalTime += curr.getTime();
+		totalOperation++;
+		for (int i = currentRoom; i < amountRooms; i++) {
+			currentRoom = i;
+			if (rooms[i].addOperation(curr)) {
+				i = amountRooms;	//break
+				bookedTime += curr.getTime();
+				bookedOperation++;
+			}
+		}
+	}
+	processTime = clock() - processTime;
 }
 
-void Schedule::nextFit(List<Operation>& list, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
+void Schedule::firstFit()
+{
+	Operation curr;
+
+	totalTime = 0;
+	totalOperation = 0;
+	bookedTime = 0;
+	bookedOperation = 0;
+
+	processTime = clock();
+	int index = 0;
+	int length = list.length();
+	while ((datatype == DataType::HEAP ? !queue.isEmpty() : index < length)) {
+		curr = (datatype == DataType::HEAP ? queue.dequeue() : list.getAt(index++));
+		totalTime += curr.getTime();
+		totalOperation++;
+		for (int i = 0; i < amountRooms; i++) {
+			if (rooms[i].addOperation(curr)) {
+				i = amountRooms;	//break
+				bookedTime += curr.getTime();
+				bookedOperation++;
+			}
+		}
+	}
+	processTime = clock() - processTime;
 }
 
-void Schedule::firstFit(PriorityQueue<Operation>& queue, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
-}
+void Schedule::bestFit()
+{
+	Operation curr;
 
-void Schedule::firstFit(List<Operation>& list, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
-}
+	totalTime = 0;
+	totalOperation = 0;
+	bookedTime = 0;
+	bookedOperation = 0;
 
-void Schedule::bestFit(PriorityQueue<Operation>& queue, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
-}
+	processTime = clock();
+	//best fit decreasing
+	int index = 0;
+	int length = list.length();
+	while ((datatype == DataType::HEAP ? !queue.isEmpty() : index < length)) {
+		curr = (datatype == DataType::HEAP ? queue.dequeue() : list.getAt(index++));
+		totalTime += curr.getTime();
+		totalOperation++;
 
-void Schedule::bestFit(List<Operation>& list, const int amountRooms, Bin * rooms, int & totalTime, int & totalOperation, int & bookedTime, int & bookedOperation, clock_t & processTime) {
+		int minIndex = -1;
+		int minValue = INT_MAX;
+		for (int i = 0; i < amountRooms; i++) {
+			if (rooms[i].remainingSize() - curr.getTime() < minValue && rooms[i].remainingSize() - curr.getTime() >= 0) {
+				minIndex = i;
+			}
+		}
+		if (minIndex != -1) {
+			rooms[minIndex].addOperation(curr);
+			bookedOperation++;
+		}
+	}
+
+	processTime = clock() - processTime;
 }
